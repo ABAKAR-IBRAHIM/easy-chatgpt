8afd177350364714652733b1b3498810ef836c0b
ABAKAR IBRAHIM <abakarib7@gmail.com>
2023-05-18 22:59:05 +0100

    API integration completed

---
+++ app/api/chat/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+export async function POST(request: Request) {
+  const res = await request.json();
+  const options = {
+    method: 'POST',
+    url: process.env.URL,
+    headers: {
+      'Accept-Encoding': 'gzip',
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': process.env.X_RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.X_RAPID_API_HOST
+    },
+    data: res
+  };
+  
+  try {
+    const response = await axios.request(options);
+    
+ 
+  return NextResponse.json(response.data.choices[0].message.content);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error);
+  }
+  
+ 
+  
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl bg-gray-900   top-0 '>
+        <div className='flex justify-between items-center w-full h-full px-4 2xl:px-16'>
+            <div className='text-white font-mono font-bold'>
+            Easy Chatgpt
+            </div>
+   
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
