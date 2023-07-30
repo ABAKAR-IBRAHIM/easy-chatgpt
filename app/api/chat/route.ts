2ffe5ab038fa736eb537a49dba6cada9e715579e
ABAKAR IBRAHIM <abakarib7@gmail.com>
2023-07-30 20:58:33 +0100

    Changed the Api end point to a better one

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,31 +1,30 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
+import { NextResponse } from "next/server";
+import axios from "axios";
 export async function POST(request: Request) {
   const res = await request.json();
   const options = {
-    method: 'POST',
+    method: "POST",
     url: process.env.URL_,
     headers: {
-      'Accept-Encoding': 'gzip',
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': process.env.X_RAPID_API_KEY,
-      'X-RapidAPI-Host': process.env.X_RAPID_API_HOST
+      "Accept-Encoding": "gzip",
+      "content-type": "application/json",
+      "X-RapidAPI-Key": process.env.X_RAPID_API_KEY,
+      "X-RapidAPI-Host": process.env.X_RAPID_API_HOST,
     },
-    data: res
+    data: res,
   };
-  
+
   try {
     const response = await axios.request(options);
-    
- 
-  return NextResponse.json(response.data.choices[0].message.content);
+
+    return NextResponse.json(response.data);
   } catch (error) {
-    console.error(error );
-    return   new Response(JSON.stringify({ error: { message: (error as Error).message} }), {
-      status: 400,
-    });
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: { message: (error as Error).message } }),
+      {
+        status: 400,
+      }
+    );
   }
-  
- 
-  
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,96 +4,80 @@ import Navbar from "@/components/navbar";
 import { useState } from "react";
 
 export default function Home() {
-	
-	const [loading, setLoading] = useState(false);
-	const [isResOk, setIsResOk] = useState(true);
-	const [errMessage, setErrMessage] = useState('Something went wrong');
-	const [content, setContent] = useState('Hello! How may I assist you today?');
-	async function handleSubmit(event: any) {
-		event.preventDefault();
-		setLoading(true);
-		 
-		const data ={
-			model: process.env.NEXT_PUBLIC_MODEL,
-    messages: [
-      {
-        role: process.env.NEXT_PUBLIC_ROLE,
-        content: event.target.message.value
- 
-      }
-    ]
-  
-		  }
+  const [loading, setLoading] = useState(false);
+  const [isResOk, setIsResOk] = useState(true);
+  const [errMessage, setErrMessage] = useState("Something went wrong");
+  const [content, setContent] = useState("Hello! How may I assist you today?");
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+    setLoading(true);
 
-		let response = await fetch("api/chat", {
-			method: "POST",
-			
-			body: JSON.stringify(data),
-		});
-		
-		if(!response.ok){
-			let res=await response.json();
-			
-			setErrMessage(res.error?.message??'Something went wrong')
-			setIsResOk(response.ok)
-		}
-		if (response.ok) {
-			let res=await response.json();
-			console.log(`Message sent successfully ${res}`);
-			
+    const data = {
+      // model: process.env.NEXT_PUBLIC_MODEL,
+      messages: [
+        {
+          role: process.env.NEXT_PUBLIC_ROLE,
+          content: event.target.message.value,
+        },
+      ],
+    };
 
-			
-			setLoading(false);
-			setContent(res.replace(/\n/g, "<br />"))
-			
-			event.target.message.value = "";
-		}
+    let response = await fetch("api/chat", {
+      method: "POST",
 
-		
+      body: JSON.stringify(data),
+    });
 
-	setLoading(false);
+    if (!response.ok) {
+      let res = await response.json();
 
-		
-	
-		
-	}
+      setErrMessage(res.error?.message ?? "Something went wrong");
+      setIsResOk(response.ok);
+    }
+    if (response.ok) {
+      let res = await response.json();
+      console.log(`Message sent successfully ${res}`);
 
+      setLoading(false);
+      setContent(res.LLAMA.replace(/\n/g, "<br />"));
 
-	if(!isResOk) throw new Error(errMessage)
+      event.target.message.value = "";
+    }
 
+    setLoading(false);
+  }
 
-	return (
-		
+  if (!isResOk) throw new Error(errMessage);
 
-		
-		<>
-		<Navbar />
-		
-		<p  dangerouslySetInnerHTML={{__html:content}} className="text-white pt-32 font-mono pl-5 pr-5" />
-	
-		<form onSubmit={handleSubmit}className="absolute inset-x-0  pl-14 pr-14 bottom-0" >
-		
-		
-			<div>
-				
-				<textarea
-					rows={4}
-					required
-					
-					name="message"
-					className="w-full p-4 bg-gray-700 border border-gray-500 text-white"
-				/>
-			</div>
-			<button
-				type="submit"
-				disabled={loading}
-				className="px-4 py-2 w-40 bg-gray-700 disabled:bg-gray-400 disabled:text-gray-100 text-white font-medium mt-4 font-mono">
-				Send 
-			</button>
-		</form>
-		
-		</>
-		
-	);
+  return (
+    <>
+      <Navbar />
+
+      <p
+        dangerouslySetInnerHTML={{ __html: content }}
+        className="text-white pt-32 font-mono pl-5 pr-5"
+      />
+
+      <form
+        onSubmit={handleSubmit}
+        className="absolute inset-x-0  pl-14 pr-14 bottom-0"
+      >
+        <div>
+          <textarea
+            rows={4}
+            required
+            name="message"
+            className="w-full p-4 bg-gray-700 border border-gray-500 text-white"
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={loading}
+          className="px-4 py-2 w-40 bg-gray-700 disabled:bg-gray-400 disabled:text-gray-100 text-white font-medium mt-4 font-mono"
+        >
+          Send
+        </button>
+      </form>
+    </>
+  );
 }
-
